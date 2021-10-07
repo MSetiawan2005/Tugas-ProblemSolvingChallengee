2d5c18bbdc07133caa75bcc5615ca6a1cf09024e
MSetiawan2005 <ms0132005@gmail.com>
2021-10-08 06:00:24 +0700

    upld problem 6

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float kecepatan;
+    public Vector2 gerakan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gerakan.x = Input.GetAxisRaw("Horizontal");
+        gerakan.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + gerakan * kecepatan * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/RandomSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+	public Transform[] Positions;
+	public GameObject Object;
+
+	public Transform Location;
+
+	public bool ToSpawn = true;
+
+	// Start is called before the first frame update
+	void Update()
+	{
+		Location = Positions[Random.Range(0, Positions.Length)];
+
+		if (ToSpawn == true)
+		{
+			Instantiate(Object, Location);
+			ToSpawn = false;
+			StartCoroutine(ToSpawnTrue());
+		}
+	}
+
+	IEnumerator ToSpawnTrue()
+	{
+		yield return new WaitForSeconds(0.75f);
+		ToSpawn = true;
+	}
+}
