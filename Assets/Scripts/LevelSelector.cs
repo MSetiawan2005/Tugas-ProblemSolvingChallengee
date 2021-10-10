a290834644c0cb32429af7adce613caab04c11e6
MSetiawan2005 <ms0132005@gmail.com>
2021-10-11 01:48:43 +0700

    Upload Problem 10

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelSelector : MonoBehaviour
+{
+    public GameObject levelHolder;
+    public GameObject levelIcon;
+    public GameObject thisCanvas;
+    public int numberOfLevels = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/manager/ProblemSelect.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ProblemSelect : MonoBehaviour
+{
+    [SerializeField] private Button button1;
+    [SerializeField] private Button button2;
+    [SerializeField] private Button button3;
+    [SerializeField] private Button button4;
+    [SerializeField] private Button button5;
+    [SerializeField] private Button button6;
+    [SerializeField] private Button button7;
+    [SerializeField] private Button button8;
+    [SerializeField] private Button button9;
+
+    private void Start()
+    {
+        button1.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+
+        button2.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(2);
+        });
+
+        button3.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(3);
+        });
+
+        button4.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(4);
+        });
+
+        button5.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(5);
+        });
+
+        button6.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(6);
+        });
+
+        button7.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(7);
+        });
+
+        button8.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(8);
+        });
+
+        button9.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(9);
+        });
+    }
+}
