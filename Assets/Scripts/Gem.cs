75d2e8bdb8a8fc0907f3f342731f873080586eb7
MSetiawan2005 <ms0132005@gmail.com>
2021-10-15 20:01:27 +0700

    problem 9

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+
+    public Texture2D cursorArrow;
+ 
+
+    void Start()
+    {
+     
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+ 
+}
