7b428039c028ac1bc8c17ff0788ad3a089ba970e
MSetiawan2005 <ms0132005@gmail.com>
2021-10-16 14:02:41 +0700

    ....

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject GameOver;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            Destroy(collision.gameObject);
+            GameOver.SetActive(true);
+        }
+    }
+}
