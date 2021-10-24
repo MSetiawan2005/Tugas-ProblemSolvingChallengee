f4145676a328950ed6da2740969782de1bcfa7c8
MSetiawan2005 <ms0132005@gmail.com>
2021-10-24 12:52:59 +0700

    updt

---
+++ Assets/TimerSet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerSet : MonoBehaviour
+{
+
+    public Text TextTimer;
+    public float waktu = 100;
+
+    public bool gameAktif = true;
+    public GameObject gameover;
+    void SetText()
+    {
+        int Menit = Mathf.FloorToInt(waktu / 60);
+        int Detik = Mathf.FloorToInt(waktu % 60);
+
+        TextTimer.text = Menit.ToString("00") +":"+ Detik.ToString("00");
+    }
+
+    float s;
+
+    private void Update()
+    {
+        if (gameAktif)
+        {
+            s += Time.deltaTime;
+            if (s >= 1)
+            {
+                waktu--;
+                s = 0;
+            }
+        }
+      
+        if(gameAktif && waktu <= 0)
+        {
+            Debug.Log("game over");
+            gameover.SetActive(true);
+            gameAktif = false;
+        }
+        SetText();
+
+        
+    }
+}
